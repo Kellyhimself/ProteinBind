8216cf2c81da1d1702ec7d3a4465f6ebb4e9302b
Kitui Kelly <kituikelly@gmail.com>
2024-09-08 10:41:56 +0300

    modifies nextauth

--- src/app/auth-page/signin/page.tsx
+++ src/app/auth-page/signin/page.tsx
@@ -95,7 +95,7 @@ const SignIn: React.FC = () => {
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
                       placeholder="6+ Characters, 1 Capital letter"
-                      className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                       required
                       disabled={isLoading}
                     />
